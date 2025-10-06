8d8bc3165d17ad54ddd66d7c5bd0562aaebe97aa
Your Name <you@example.com>
2025-10-06 08:16:08 -0300

    create-controller-account-pipes-env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { envSchema } from './env';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [ConfigModule.forRoot({
+    validate: (env)=> envSchema.parse(env),
+    isGlobal: true
+  })],
+  controllers: [CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,40 @@
+import { ConflictException, UsePipes } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { hash } from 'bcryptjs';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipes';
+import { PrismaService } from 'src/prisma/prisma.service';
+import {email, z} from 'zod';
+const createAccountBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string()
+})
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  @HttpCode(201)
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = body;
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'user with same email address already exists',
+      );
+    }
+    const hashedPassord =await hash(password, 8)
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassord,
+      },
+    });
+  }
+}
